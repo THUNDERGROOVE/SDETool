6123c6428842f8bf8e427706b4d3b3ab0fee014e
Nick Powell <markusthefish@gmail.com>
2015-05-05 01:31:15 -0700

    Setup our static site

--- Makefile
+++
@@ -1,2 +0,0 @@
-all:
-	go build -ldflags "-X main.Commit `git rev-parse --short HEAD` -X main.Branch `git rev-parse --abbrev-ref HEAD` -X main.Version `git describe --abbrev=0`"
--- hello.lua
+++
@@ -1,4 +0,0 @@
-print("Hello SDETool\nLet's open an SDE version")
-sde.loadVersion("1.0-WL")
-t = sde.getTypeByID(364022)
-print(t.Attributes.mDisplayName.." contains "..#t.Attributes.. " attributes")
