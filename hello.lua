fb9f0aa70611ddeb08dcf34841df6c48f4d0ef9f
Nick Powell <markusthefish@gmail.com>
2015-05-07 15:43:01 -0700

    Updated hello.lua example script

--- hello.lua
+++ hello.lua
@@ -1,4 +1,20 @@
 print("Hello SDETool\nLet's open an SDE version")
-sde.loadVersion("1.0-WL")
+
+
+print("Pick a verison:")
+v = sde.getVersions()
+
+for i = 1, #v do
+	print("  "..i..") "..v[i])
+end
+
+while true do
+	io.write("> ")
+	ver = tonumber(io.read())
+	if ver == nil then print("Invalid input") else break end
+end
+
+sde.loadVersion(v[ver])
+
 t = sde.getTypeByID(364022)
 print(t.Attributes.mDisplayName.." contains "..#t.Attributes.. " attributes")
