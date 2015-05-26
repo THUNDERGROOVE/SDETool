6e4f9710d82d1a58575b1b94c9656d23bfaea9a3
Nick Powell <markusthefish@gmail.com>
2015-05-26 15:17:48 -0700

    Re add search command

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 	}
 
 	// @TODO:
-	//	Removing kingpin for our own command parser
+	//	Finish implementing all the old commands and flags
 
 	c := strings.Join(os.Args[1:], " ")
 	s := args.NewScanner(strings.NewReader(c))
@@ -55,6 +55,30 @@ func main() {
 			fmt.Printf("Token %v; Literal: '%v'\n", v.Token, v.Literal)
 		}
 	}
+
+	args.FlagReg.RegisterCmd("search", func(tok args.TokLit, index int) {
+		if SDE == nil {
+			fmt.Printf("NO SDE file was loaded\n")
+			return
+		}
+		if arg := tl.Next(index); arg != nil {
+			switch arg.Token {
+			case args.STRING:
+				fmt.Printf("Searching using string '%v'\n", arg.Literal)
+				out, err := SDE.Search(arg.Literal)
+				if err != nil {
+					fmt.Printf("Error searching the SDE: %v\n", err.Error())
+				}
+				for _, v := range out {
+					fmt.Printf("  [%v] [%v]\n", v.TypeID, v.GetName())
+				}
+			default:
+				fmt.Printf("search doesn't take a token type of %v\n", arg.Token)
+			}
+		} else {
+			fmt.Printf("No argument was supplied for search\n")
+		}
+	})
 
 	args.FlagReg.RegisterCmd("lookup", func(tok args.TokLit, index int) {
 		if SDE == nil {
@@ -77,7 +101,7 @@ func main() {
 				fmt.Printf("Lookup doesn't know how to handle token %v\n", arg.Token)
 			}
 		} else {
-			fmt.Printf("No argument supplied for lookup")
+			fmt.Printf("No argument supplied for lookup\n")
 		}
 		if Type == nil {
 			fmt.Printf("Type returned was nil?\n")
@@ -115,100 +139,9 @@ func main() {
 
 	args.FlagReg.Parse(tl)
 
-	/*
-		switch kingpin.MustParse(args.SDETool.Parse(os.Args[1:])) {
-		case args.ListLangs.FullCommand():
-			fmt.Println("Compiled in languages: ")
-			for k, _ := range langs.ScriptingLangs {
-				fmt.Printf("  %v\n", k)
-			}
-		case args.DoScript.FullCommand():
-			if err := langs.RunScript(*args.DoScriptLang, *args.DoScriptFile); err != nil {
-				if err == langs.NoSuchLang {
-					fmt.Printf("The language provided '%v' is not compiled in. \n", *args.DoScriptLang)
-					return
-				} else {
-					fmt.Printf("The following error occured while running the script '%v': %v", *args.DoScriptFile, err.Error())
-				}
-			}
-		case args.Interpreter.FullCommand():
-			if err := langs.Interpreter(*args.InterpreterLang); err != nil {
-				if err == scripting.InterpreterNotImplemented {
-					fmt.Printf("The language %v does not implement an interpreter.\n", *args.InterpreterLang)
-				} else {
-					fmt.Printf("The following error occured while in the interpreter %v\n", err.Error())
-				}
-			}
-		case args.Lookup.FullCommand():
-			if *args.SDEFile == "" {
-				fmt.Println("You must supply an SDEFile using the sde flag")
-				return
-			}
-			SDE, err := sde.Load(*args.SDEFile)
-			if err != nil {
-				fmt.Println("Error while opening the SDE file")
-				return
-			}
-			fmt.Printf("SDE file opened using roughly %v bytes of memory\n", SDE.Size())
-			if *args.LookupTID != 0 {
-				t, err := SDE.GetType(*args.LookupTID)
-				if err == nil {
-					Type = t
-				} else {
-					fmt.Println("Couldn't find the type:", *args.LookupTID)
-					os.Exit(1)
-				}
-			}
-			if Type != nil {
-				if *args.LookupAttr {
-					fmt.Printf("Attributes for:\n  %v | %v | %v\n", Type.GetName(), Type.TypeID, Type.TypeName)
-					cleanPrintAttrs(Type)
-				}
-				if *args.TypeRefers {
-					fmt.Printf("Looking up all types that refer to %v\n", Type.GetName())
-					refs, _ := SDE.FindTypesThatReference(Type)
-					if len(refs) != 0 {
-						for _, v := range refs {
-							fmt.Printf("  [%v] | %v\n", v.TypeID, v.GetName())
-						}
-					} else {
-						fmt.Printf("Found no types that refer to %v\n", Type.GetName())
-					}
-				}
-			} else {
-				fmt.Println("Failed to resolve a type.")
-			}
-		case args.Search.FullCommand():
-			if *args.SDEFile == "" {
-				fmt.Println("You must supply an SDEFile using the sde flag")
-				return
-			}
-			SDE, err := sde.Load(*args.SDEFile)
-			if err != nil {
-				fmt.Println("Error while opening the SDE file")
-				return
-			}
-			fmt.Printf("SDE file opened using roughly %v bytes of memory\n", SDE.Size())
-			if *args.SearchString != "" {
-				if *args.SearchQuick {
-					fmt.Println("Quick search not implemented yet")
-				}
-				fmt.Println("Searching for:", *args.SearchString)
-				types, err := SDE.Search(*args.SearchString)
-				if err != nil {
-					fmt.Println("Error searching for types", err.Error())
-					os.Exit(1)
-				}
-				for _, v := range types {
-					fmt.Printf("  %v | %v\n", v.TypeID, v.GetName())
-				}
-			}
-		default:
-			fmt.Println("SDETool written by Nick Powell; @THUNDERGROOVE")
-			fmt.Printf("Version %v branch %v commit %v\n", Version, Branch, Commit)
-			fmt.Println("Do --help for help")
-		}
-	*/
+	fmt.Println("SDETool written by Nick Powell; @THUNDERGROOVE")
+	fmt.Printf("Version %v branch %v commit %v\n", Version, Branch, Commit)
+	fmt.Println("Do --help for help")
 }
 
 func cleanPrintAttrs(t *sde.SDEType) {
@@ -257,7 +190,6 @@ func cleanPrintAttrs(t *sde.SDEType) {
 						v,
 					)
 				}
-				fmt.Print("\n")
 			}
 		} else {
 			fmt.Printf("  %v> %v\n",
