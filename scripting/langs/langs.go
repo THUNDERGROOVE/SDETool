1df5cb8ed77fe8e1f080993d35a247088e252981
Nick <markusthefish@gmail.com>
2015-06-26 14:55:11 -0700

    Even more vetting

--- scripting/langs/langs.go
+++ scripting/langs/langs.go
@@ -2,51 +2,54 @@ package langs
 
 import (
 	"errors"
+
 	"github.com/THUNDERGROOVE/SDETool/scripting"
 	"github.com/THUNDERGROOVE/SDETool/util/log"
 )
 
+// ScriptingLangs is a global map that contains all of out registered scripting
+// languages
 var ScriptingLangs = make(map[string]scripting.ScriptingLang)
 
-var NoSuchLang = errors.New("No such language registered")
+// ErrNoSuchLang is called when a language interface function is called on a
+// language that isn't registered or doesn't exist.
+var ErrNoSuchLang = errors.New("No such language registered")
 
+// Register registers a scripting language for use
 func Register(name string, lang scripting.ScriptingLang) {
 	log.Println("[LANGS] Registering", name)
 	ScriptingLangs[name] = lang
 }
 
+// RunString runs the string for the scripting language supplied
 func RunString(lang, s string) error {
 	if v, ok := ScriptingLangs[lang]; ok {
 		if !v.IsInit() {
 			v.Init()
 		}
 		return v.RunString(s)
-	} else {
-		return NoSuchLang
 	}
-	return nil
+	return ErrNoSuchLang
 }
 
+// RunScript runs the script for the given file
 func RunScript(lang, filename string) error {
 	if v, ok := ScriptingLangs[lang]; ok {
 		if !v.IsInit() {
 			v.Init()
 		}
 		return v.RunScript(filename)
-	} else {
-		return errors.New(NoSuchLang.Error() + ": '" + lang + "'")
 	}
-	return nil
+	return errors.New(ErrNoSuchLang.Error() + ": '" + lang + "'")
 }
 
+// Interpreter should open an interpreter for the provided language
 func Interpreter(lang string) error {
 	if v, ok := ScriptingLangs[lang]; ok {
 		if !v.IsInit() {
 			v.Init()
 		}
 		return v.Interpreter()
-	} else {
-		return NoSuchLang
 	}
-	return nil
+	return ErrNoSuchLang
 }
